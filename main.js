ad695c470f67fcf875b1fd7d3b223021d812b740
thetoothless <ninaling@ucla.edu>
2015-11-02 22:49:26 -0800

    added a comment

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ library = require('./module.js');
 console.log(library.anvGenerator());
 console.log(library.anvGenerator("V"));
 console.log(library.anvGenerator("D"));
-
+//comment
 console.log(library.countPassGenerator("gr", "facebook"));
 console.log(library.countPassGenerator(library.anvGenerator("D"), "github"));
 console.log(library.countPassGenerator("keyword", "gmail"));
